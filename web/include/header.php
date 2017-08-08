c03248ae5d71f87f15f445a24fa1bf7da0dccf5e
jokytalki3 <jokytalki3@gmail.com>
2017-08-08 11:51:58 +0800

    Add files via upload
    footer header index.php product.php

---
+++ web/include/header.php
@@ -0,0 +1,159 @@
+<div class="header">
+  <div class="container">
+    <div class="header-top">
+      <div class="logo"> <a href="index.php">N-AIR</a> </div>
+      <div class="login-bars"> <a class="btn btn-default log-bar" href="register.php" role="button">Sign up</a> <a class="btn btn-default log-bar" href="signup.php" role="button">Login</a>
+        <div class="cart box_1"> <a href="checkout.html">
+          <h3>
+            <div class="total"> <span class="simpleCart_total"></span>(<span id="simpleCart_quantity" class="simpleCart_quantity"></span>)</div>
+          </h3>
+          </a>
+          <p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+    </div>
+    <!---menu-----bar--->
+    <div class="header-botom">
+      <div class="content white">
+        <nav class="navbar navbar-default nav-menu" role="navigation">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          </div>
+          <!--/.navbar-header-->
+          
+          <div class="collapse navbar-collapse collapse-pdng" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav nav-font">
+              <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sports Equipment<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="products.php">Game Ball</a></li>
+                  <li><a href="products.php">Racket</a></li>
+                  <li><a href="products.php">Table Tennis</a></li>
+                  <li><a href="products.php">Hockey</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Running</a></li>
+                  <li><a href="products.php">Track &#38; Field</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Indoor Game</a></li>
+                  <li><a href="products.php">Outdoor Game</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Net Needle &#38; Trolley</a></li>
+                  <li><a href="products.php">Post, Board,  Ring &#38; Stand</a></li>
+                  <li><a href="products.php">Supportive</a></li>
+                  <li><a href="products.php">Accessories</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Garment<strong class="caret"></strong></a>
+                <ul class="dropdown-menu multi-column columns-2">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <ul class="multi-column-dropdown">
+                        <li><a href="products.php">Round Neck Shirt</a></li>
+                        <li><a href="products.php">Collar Neck Shirt</a></li>
+                        <li><a href="products.php">Zip Mock Neck</a></li>
+                        <li><a href="products.php">Jersey</a></li>
+                        <li class="divider"></li>
+                        <li><a href="products.php">Pant</a></li>
+                        <li class="divider"></li>
+                        <li><a href="products.php">Muslimah</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm divider"></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Corporate Uniform</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Lab Coat</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Jacket</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Vest</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Coverall</a></li>
+                        <li class="divider"></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Apron</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Cap</a></li>
+                        <li class="hidden-lg hidden-md hidden-sm"><a href="products.php">Towel</a></li>
+                      <!--long and short sleeve should be categorized later on product page-->
+                      </ul>
+                    </div>
+                    <div class="col-sm-6">
+						<ul class="multi-column-dropdown">
+                       		<li class="hidden-xs"><a href="products.php">Corporate Uniform</a></li>
+                        	<li class="hidden-xs"><a href="products.php">Lab Coat</a></li>
+                       		<li class="hidden-xs"><a href="products.php">Jacket</a></li>
+                        	<li class="hidden-xs"><a href="products.php">Vest</a></li>
+                        	<li class="hidden-xs"><a href="products.php">Coverall</a></li>
+                        	<li class="divider"></li>
+                        	<li class="hidden-xs"><a href="products.php">Apron</a></li>
+                        	<li class="hidden-xs"><a href="products.php">Cap</a></li>
+                        	<li class="hidden-xs"><a href="products.php">Towel</a></li>
+						</ul>
+					</div>
+                    <div class="col-sm-4 menu-img-pad hidden"> <a href="#"><img src="images/menu2.jpg" alt="/" class="img-rsponsive men-img-wid" /></a> </div>
+                  </div>
+                </ul>
+              </li>
+              <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Trophy<b class="caret"></b></a>
+                <ul class="dropdown-menu multi-column columns-2">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <ul class="multi-column-dropdown">
+                        <li><a href="products.php">Imported Cup</a></li>
+                        <li><a href="products.php">Hanging Medal</a></li>
+                        <li><a href="products.php">Plastic Standing Medal</a></li>
+                        <li class="divider"></li>
+                        <li><a href="products.php">Plaque</a></li>
+                        <li><a href="products.php">Pewter Award</a></li>
+                        <li><a href="products.php">Poly Resin Trophy</a></li>
+                        <li><a href="products.php">Acrylic Make-up Trophy</a></li>
+                        <li><a href="products.php">Casting Part Trophy</a></li>
+                        <li><a href="products.php">Ps Trophy</a></li>
+                        <li class="divider"></li>
+                        <li><a href="products.php">Custom Made</a></li>
+                      </ul>
+                    </div>
+                    <div class="col-sm-6"> <a href="#"><img src="images/menu3.jpg" alt="/" class="img-rsponsive men-img-wid" /></a> </div>
+                  </div>
+                </ul>
+              </li>
+              <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gift &#38; Premium<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="products.php">USB Drive Series</a></li>
+                  <li><a href="products.php">Power Bank Series</a></li>
+                  <li><a href="products.php">Pen Series</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Bag Series</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Household Series</a></li>
+                  <li><a href="products.php">Stationery Series</a></li>
+                  <li><a href="products.php">Electronic Series</a></li>
+                  <li><a href="products.php">Eco Series</a></li>
+                  <li><a href="products.php">Miscellaneous Series</a></li>
+                  <li><a href="products.php">Lanyard Series</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Custom Made<b class="caret"></b></a>
+               	<ul class="dropdown-menu">
+                  <li><a href="products.php">Embroidery</a></li>
+                  <li><a href="products.php">Heatpress</a></li>
+                  <li><a href="products.php">Silkscreeb</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Sticker</a></li>
+                  <li class="divider"></li>
+                  <li><a href="products.php">Garment</a></li>
+                  <li><a href="products.php">Trophy</a></li>
+                  <li><a href="products.php">Name Card</a></li>
+                  <li><a href="products.php">Banner</a></li>
+                  <li><a href="products.php">Bunting</a></li>
+                  <li><a href="products.php">Pennants</a></li>
+                  <li><a href="products.php">Mug</a></li>
+                  <li><a href="products.php">Chop</a></li>
+                </ul>
+                
+              </li>
+              <li class="hidden"><a href="contact.html">Custom Made</a></li>
+              <li class="hidden"><a href="#">FAQ</a></li>
+            </ul>
+          </div>
+          <!--/.navbar-collapse--> 
+        </nav>
+        <!--/.navbar--> 
+      </div>
+      <!--/.content---> 
+    </div>
+    <!--header-bottom--> 
+  </div>
+</div>
